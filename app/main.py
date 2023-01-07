f1440eb87c82499670470135f046cbf6449ed5cd
Ilya Bogretsov <ilyabogretsov@gmail.com>
2023-01-07 14:56:18 +0300

    Remove temporary unused pagination

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
-from fastapi_pagination import add_pagination
 
 from app import api
 
 
 app = FastAPI()
 app.include_router(api.api_router)
-
-add_pagination(app)
 
 
 @app.get(
